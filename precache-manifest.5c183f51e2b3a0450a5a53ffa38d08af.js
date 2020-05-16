61e8fc67bf87d91a0945b1ce1cbbb6c2167b8252
sachinraaut <sachinr1013@gmail.com>
2020-05-16 16:26:01 +0530

    Updates

---
+++ precache-manifest.5c183f51e2b3a0450a5a53ffa38d08af.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "27e452a0fed064771237754f89dde15e",
+    "url": "/create-react-app/index.html"
+  },
+  {
+    "revision": "7bdebf8a69b694e9e184",
+    "url": "/create-react-app/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "5180f10d33888ef08a4b",
+    "url": "/create-react-app/static/js/2.ddaaefff.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/create-react-app/static/js/2.ddaaefff.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7bdebf8a69b694e9e184",
+    "url": "/create-react-app/static/js/main.777b91d6.chunk.js"
+  },
+  {
+    "revision": "da69e42e948f8931bcff",
+    "url": "/create-react-app/static/js/runtime-main.951cf345.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/create-react-app/static/media/logo.5d5d9eef.svg"
+  }
+]);
